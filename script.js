d9993ee928e8a8d17b3918a1831793878d4a80b1
Inventicon <conn@widtfeldt.com>
2015-12-16 18:25:56 -0700

    Added more stuff and SORTING! YAY!
    SORTING!

---
+++ script.js
@@ -0,0 +1,89 @@
+function change(prio, price)
+{
+    $(".item").css("height", "0px");
+    
+    $(".item").each(function(i, obj) {
+        
+        if (prio != 0 && price != 0)
+        {
+            if ($(this).hasClass(prio) && $(this).hasClass(price))
+            {
+                $(this).css("height", "128px");
+            }
+        }
+        else if (prio == 0 && price != 0)
+        {
+            if ($(this).hasClass(price))
+            {
+                $(this).css("height", "128px");
+            }
+        }
+        else if (prio != 0 && price == 0)
+        {
+            if ($(this).hasClass(prio))
+            {
+                $(this).css("height", "128px");
+            }
+        }
+        else {
+            $(".item").css("height", "128px");
+        }
+    });
+}
+
+$( document ).ready(function() {
+    var prio = 0;
+    var price = 0;
+    
+    change(prio, price);
+    
+    $("input[name$='priority']").click(function() {
+        var value = $(this).attr("id");
+        if (value == "all-priority-button")
+            {
+                prio = 0;
+            }
+        else if (value == "extreme-priority-button")
+            {
+                prio = "extreme-priority";
+            }
+        else if (value == "high-priority-button")
+            {
+                prio = "high-priority";
+            }
+        else if (value == "medium-priority-button")
+            {
+                prio = "medium-priority";
+            }
+        else if (value == "low-priority-button")
+            {
+                prio = "low-priority";
+            }
+        change(prio, price);
+    }); 
+    
+    $("input[name$='price']").click(function() {
+        var value = $(this).attr("id");
+        if (value == "all-price-button")
+            {
+                price = 0;
+            }
+        else if (value == "expensive-price-button")
+            {
+                price = "expensive";
+            }
+        else if (value == "high-price-button")
+            {
+                price = "high";
+            }
+        else if (value == "medium-price-button")
+            {
+                price = "medium";
+            }
+        else if (value == "low-price-button")
+            {
+                price = "cheap";
+            }
+        change(prio, price);
+    });
+});
